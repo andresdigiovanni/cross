5761a568f56697c79e50f31ba549e1fa98589c3d
Andrés Di Giovanni <andresdigiovanni@gmail.com>
2025-03-12 14:11:31 +0100

    refactor auto_transform

--- cross/auto_transform.py
+++ cross/auto_transform.py
@@ -1,28 +1,50 @@
 import warnings
+from collections import defaultdict
 from datetime import datetime
 from typing import Callable, List, Optional, Union
 
 import numpy as np
 
-from cross.auto_parameters import (
-    CategoricalEncodingParamCalculator,
-    ColumnSelectionParamCalculator,
-    CyclicalFeaturesTransformerParamCalculator,
-    DateTimeTransformerParamCalculator,
-    DimensionalityReductionParamCalculator,
-    MathematicalOperationsParamCalculator,
-    MissingValuesIndicatorParamCalculator,
-    MissingValuesParamCalculator,
-    NonLinearTransformationParamCalculator,
-    NormalizationParamCalculator,
-    NumericalBinningParamCalculator,
-    OutliersParamCalculator,
-    QuantileTransformationParamCalculator,
-    ScaleTransformationParamCalculator,
-    SplineTransformationParamCalculator,
-)
+import cross.auto_parameters as pc
 from cross.transformations.utils.dtypes import numerical_columns
 from cross.utils import get_transformer
+
+
+def date_time() -> str:
+    return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+
+def execute_transformation(
+    calculator,
+    X,
+    y,
+    model,
+    scoring,
+    direction,
+    cv,
+    groups,
+    verbose,
+    transformations,
+    subset=None,
+):
+    if verbose:
+        print(
+            f"\n[{date_time()}] Fitting transformation: {calculator.__class__.__name__}"
+        )
+
+    initial_columns = set(X.columns)
+    X_subset = X.loc[:, subset] if subset else X
+
+    transformation = calculator.calculate_best_params(
+        X_subset, y, model, scoring, direction, cv, groups, verbose
+    )
+
+    if transformation:
+        transformations.append(transformation)
+        transformer = get_transformer(transformation["name"], transformation["params"])
+        X = transformer.fit_transform(X, y)
+
+    return X, list(set(X.columns) - initial_columns)
 
 
 def auto_transform(
@@ -51,121 +73,83 @@ def auto_transform(
         List[dict]: A list of applied transformations.
     """
 
-    def date_time() -> str:
-        return datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-
-    def execute_transformation(calculator, X, y, subset=None):
-        # print status
-        if verbose:
-            date_time_str = date_time()
-            func_name = calculator.__class__.__name__
-            print(f"\n[{date_time_str}] Fitting transformation: {func_name}")
-
-        # calculate best params for transformation
-        X_subset = X.loc[:, subset] if subset else X
-        transformation = calculator.calculate_best_params(
-            X_subset, y, model, scoring, direction, cv, groups, verbose
-        )
-
-        # apply transformation
-        initial_columns = X.columns
-
-        if transformation:
-            transformations.append(transformation)
-            transformer = get_transformer(
-                transformation["name"], transformation["params"]
-            )
-            X = transformer.fit_transform(X, y)
-
-        new_columns = list(set(X.columns) - set(initial_columns))
-        return X, new_columns
-
     if verbose:
-        date_time_str = date_time()
-        print(
-            f"\n[{date_time_str}] Starting experiment to find the best transformations"
-        )
-        print(f"[{date_time_str}] Data shape: {X.shape}")
-        print(f"[{date_time_str}] Model: {model.__class__.__name__}")
-        print(f"[{date_time_str}] Scoring: {scoring}\n")
+        print(f"\n[{date_time()}] Starting experiment to find the best transformations")
+        print(f"[{date_time()}] Data shape: {X.shape}")
+        print(f"[{date_time()}] Model: {model.__class__.__name__}")
+        print(f"[{date_time()}] Scoring: {scoring}\n")
 
     X = X.copy()
-    orig_num_columns = numerical_columns(X)
-
+    initial_num_columns = numerical_columns(X)
     transformations = []
 
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore")
 
-        # MissingValuesIndicator
-        calculator = MissingValuesIndicatorParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
+        additional_columns = defaultdict(list)
 
-        # MissingValuesHandler
-        calculator = MissingValuesParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
+        # transformation - subset - key
+        transformations_sequence = [
+            (pc.MissingValuesIndicatorParamCalculator, None, None),
+            (pc.MissingValuesParamCalculator, None, None),
+            (pc.OutliersParamCalculator, None, None),
+            (pc.NonLinearTransformationParamCalculator, None, None),
+            (pc.NormalizationParamCalculator, None, None),
+            (pc.QuantileTransformationParamCalculator, None, None),
+            (
+                pc.SplineTransformationParamCalculator,
+                lambda: initial_num_columns,
+                "spline",
+            ),
+            (
+                pc.MathematicalOperationsParamCalculator,
+                lambda: initial_num_columns,
+                "math",
+            ),
+            (
+                pc.NumericalBinningParamCalculator,
+                lambda: initial_num_columns + additional_columns["math"],
+                "binning",
+            ),
+            (
+                pc.ScaleTransformationParamCalculator,
+                lambda: initial_num_columns
+                + additional_columns["spline"]
+                + additional_columns["math"]
+                + additional_columns["binning"],
+                None,
+            ),
+            (pc.DateTimeTransformerParamCalculator, None, "datetime"),
+            (
+                pc.CyclicalFeaturesTransformerParamCalculator,
+                lambda: additional_columns["datetime"]
+                if additional_columns["datetime"]
+                else None,
+                None,
+            ),
+            (pc.CategoricalEncodingParamCalculator, None, None),
+            (pc.ColumnSelectionParamCalculator, None, None),
+            (pc.DimensionalityReductionParamCalculator, None, None),
+        ]
 
-        # OutliersHandler
-        calculator = OutliersParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
+        for calculator_cls, subset, key in transformations_sequence:
+            calculator = calculator_cls()
+            subset_columns = subset() if callable(subset) else subset
+            X, new_columns = execute_transformation(
+                calculator,
+                X,
+                y,
+                model,
+                scoring,
+                direction,
+                cv,
+                groups,
+                verbose,
+                transformations,
+                subset_columns,
+            )
 
-        # NonLinearTransformation
-        calculator = NonLinearTransformationParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
-
-        # Normalization
-        calculator = NormalizationParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
-
-        # QuantileTransformation
-        calculator = QuantileTransformationParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
-
-        # SplineTransformation - original num columns
-        calculator = SplineTransformationParamCalculator()
-        X, spline_columns = execute_transformation(
-            calculator, X, y, subset=orig_num_columns
-        )
-
-        # MathematicalOperations
-        calculator = MathematicalOperationsParamCalculator()
-        X, math_columns = execute_transformation(
-            calculator, X, y, subset=orig_num_columns
-        )
-
-        # NumericalBinning - original num + math columns
-        calculator = NumericalBinningParamCalculator()
-        columns_to_transform = orig_num_columns + math_columns
-        X, binning_columns = execute_transformation(
-            calculator, X, y, subset=columns_to_transform
-        )
-
-        # ScaleTransformation - original num + spline + math + binning columns
-        calculator = ScaleTransformationParamCalculator()
-        columns_to_transform = (
-            orig_num_columns + spline_columns + math_columns + binning_columns
-        )
-        X, _ = execute_transformation(calculator, X, y, subset=columns_to_transform)
-
-        # DateTimeTransformer
-        calculator = DateTimeTransformerParamCalculator()
-        X, datetime_columns = execute_transformation(calculator, X, y)
-
-        # CyclicalFeaturesTransformer - only for date time features
-        if datetime_columns:
-            calculator = CyclicalFeaturesTransformerParamCalculator()
-            X, _ = execute_transformation(calculator, X, y, subset=datetime_columns)
-
-        # CategoricalEncoding
-        calculator = CategoricalEncodingParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
-
-        # ColumnSelection
-        calculator = ColumnSelectionParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
-
-        # DimensionalityReduction
-        calculator = DimensionalityReductionParamCalculator()
-        X, _ = execute_transformation(calculator, X, y)
+            if key:
+                additional_columns[key] = new_columns
 
     return transformations
